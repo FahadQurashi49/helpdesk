d14e8f257bc6a4d3ccabce660be8e6e6db226573
Fahad Qureshi <fahad.ahmad049@gmail.com>
2024-04-23 18:12:35 +0500

    feat(tickets): add tickets page

---
+++ app/tickets/TicketList.tsx
@@ -0,0 +1,36 @@
+interface Ticket {
+    id: string;
+    title: string;
+    body: string;
+    priority: string;
+    user_email: string;
+}
+
+async function getTickets(): Promise<Ticket[]> {
+    const res = await fetch('http://localhost:4000/tickets', {
+        next: {
+            revalidate: 0
+        }
+    })
+    return res.json()
+}
+
+export default async function TicketList() {
+    const tickets: Ticket[] = await getTickets()
+  return (
+    <>
+    {tickets.map((ticket) => (
+        <div key={ticket.id} className='card my-5'>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 20)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+                {ticket.priority} priority
+            </div>
+        </div>
+    ))}
+    {tickets.length === 0 && (
+        <p className='text-center'>There are no open tickets!</p>
+    )}
+    </>
+  )
+}
